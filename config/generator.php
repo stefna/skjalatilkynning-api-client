adcea75d6cf8a1803b313d20880bb27a215f3c89
Ingimar Svanberg Jóhannesson <ingimar@stefna.is>
2024-05-30 13:00:22 +0000

    [core] Downgrade the generator to v8.0

--- config/generator.php
+++ config/generator.php
@@ -1,13 +1,13 @@
 <?php declare(strict_types=1);
 
 use SkjalatilkynningApiClient\Generator\Transformer;
-use Stefna\PhpCodeBuilder\Renderer\Php82Renderer;
+use Stefna\PhpCodeBuilder\Renderer\Php8Renderer;
 
 return [
 	'transformer' => new Transformer(),
 	'specification' => dirname(__DIR__) . '/resources/skjalatilkynning-api.json',
 	'saveLocation' => dirname(__DIR__) . '/generated',
 	'requestBodyRequiredByDefault' => true,
-	'phpRenderer' => new Php82Renderer(),
+	'phpRenderer' => new Php8Renderer(),
 ];
 
